d3087d241345180662f955b56ec1aec064a9137b
gourav-niranjan <gouravniranjan9@gmail.com>
2026-02-05 17:21:38 +0530

    changed from MySQL to PostgresSQL

--- src/main/java/com/bookShop/servlet/RegisterServlet.java
+++ src/main/java/com/bookShop/servlet/RegisterServlet.java
@@ -33,14 +33,24 @@ public class RegisterServlet extends HttpServlet {
 		
 		//Load JDBC 
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName("org.postgresql.Driver");
+
 		}catch(ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 		
 		//Generate a connection
 		try {
-			Connection c = DriverManager.getConnection("jdbc:mysql:///book", "root", "pass123");
+			String host = System.getenv("DB_HOST");
+			String db   = System.getenv("DB_NAME");
+			String user = System.getenv("DB_USER");
+			String pass = System.getenv("DB_PASSWORD");
+			String port = System.getenv("DB_PORT");
+
+			String url = "jdbc:postgresql://" + host + ":" + port + "/" + db;
+
+			Connection c = DriverManager.getConnection(url, user, pass);
+
 			PreparedStatement ps = c.prepareStatement(query);
 			ps.setString(1, bookName);
 			ps.setString(2, bookEdition);
